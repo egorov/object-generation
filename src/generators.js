5221f65f1db761f7275e0284397f206c6d5b4868
Sergey Egorov <s.e.888@live.ru>
2020-04-05 14:54:44 +0500

    Исправил дизайн, удалил ненужные генераторы

--- src/generators.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  date: require('./date_generator'),
-  email: require('./email_generator'),
-  number: require('./number_generator'),
-  string: require('./string_generator')
-};
--- src/object_generator.js
+++ src/object_generator.js
@@ -1,46 +1,25 @@
-const generators = require('./generators');
+const factories = require('./factories');
 
 function object_generator(metadata) {  
-
-  const value_store = make_value_store();
-  
+ 
   let value = null;
 
   for(const property in metadata) {
     
-    const action = {
-      type: 'metadata',
-      payload: metadata[property]
-    };
-    
-    value_store.dispatch(action);
-    
-    const generate = generators[metadata[property].type];
+    const make = factories[metadata[property].type];
 
-    if(typeof generate !== 'function')
+    if(typeof make !== 'function')
       continue;
 
-    generate(value_store);
+    const property_value = make(metadata[property]);
 
     if(value === null)
       value = {};
 
-    value[property] = value_store.getState().value;
+    value[property] = property_value;
   }
 
   return value;
-}
-
-function make_value_store() {
-  
-  const store = require('./value_factory_store').create();
-
-  const type = 'factories';
-  const payload = require('./factories');
-  
-  store.dispatch({type, payload});
-
-  return store;
 }
 
 module.exports = object_generator;
